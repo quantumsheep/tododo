f0ae26b722fac3d24fe00298dab96dfea9e5ff5c
NenetteGit <antonin.texier@gmail.com>
2020-02-19 23:21:41 +0100

    refactor(api): Adapt code to babel implementation

--- server/src/db.js
+++ server/src/db.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 
 mongoose.connect('mongodb://localhost:27017/tododo', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-module.exports = mongoose
+export default mongoose
--- server/src/utils/require_all.js
+++
@@ -1,6 +0,0 @@
-const glob = require('glob')
-
-/**
- * @param {string} directory 
- */
-module.exports = (directory) => glob.sync(directory, { nodir: true }).map(f => require(f))
