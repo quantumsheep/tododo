08b27ba827b804ad67652e46f9dd345898604558
QuantumSheep <quantumsheep@outlook.fr>
2020-02-20 14:51:27 +0100

    feat(debug): Remove useless debuging

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,10 +2,7 @@ import { Router } from 'express'
 const router = Router()
 export default router
 
-import { uri } from '../db'
-
 router.get('/api', (req, res) => {
-  console.log(uri)
   res.send({
     name: 'Tododo',
   })
