754056142569e37a173e416424d1bed6c2aef488
Simonpo95 <halimisimon@gmail.com>
2020-02-22 18:44:27 +0100

    feat(server): End checklist model

--- server/src/models/checklist.js
+++ server/src/models/checklist.js
@@ -3,5 +3,6 @@ import db from '../db'
 export const model = db.model('Checklist', {
     title: { type: String, required: true },
     checked: { type: Boolean, default: false },
+    tasks: {type: [db.Types.ObjectId], default: [] },
     created: { type: Date, default: Date.now, },
 })
