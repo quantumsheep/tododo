5692940fe4dfb2b20121d000f167ba506b601bf9
Simonpo95 <halimisimon@gmail.com>
2020-02-22 19:24:44 +0100

    feat(server): Rework checklist

--- server/src/models/checklist.js
+++ server/src/models/checklist.js
@@ -1,8 +1,15 @@
+import { Schema } from 'mongoose'
+
 import db from '../db'
 
+const Task = new Schema({
+  title: { type: String, required: true, },
+  checked: { type: Boolean, default: false, },
+})
+
 export const model = db.model('Checklist', {
-    title: { type: String, required: true },
-    checked: { type: Boolean, default: false },
-    tasks: {type: [db.Types.ObjectId], default: [] },
-    created: { type: Date, default: Date.now, },
+  title: { type: String, required: true, },
+  checked: { type: Boolean, default: false, },
+  tasks: { type: [Task], default: [], },
+  created: { type: Date, default: Date.now, },
 })
--- server/src/models/task.js
+++
@@ -1,6 +0,0 @@
-import db from '../db'
-
-export const model = db.model('Task', {
-    title: { type: String, required: true },
-    done: { type: Boolean, default: false, required: true }
-})
