951eefa71df9f20918e96a6a54d05e5b7ddd1fe3
Simonpo95 <halimisimon@gmail.com>
2020-02-22 17:42:17 +0100

    feat(client): Created simpliest possible component to test api call

---
+++ client/src/components/todolist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+    const Todolist = ({ todolist }) => {
+      return (
+        <div>
+          <center><h1>{todolist.title}</h1></center>
+        </div>
+      )
+    };
+
+    export default Todolist
