a79b81806dacada51e349dc2c90db6f8ca10ca94
NenetteGit <antonin.texier@gmail.com>
2020-02-26 00:04:56 +0100

    test(server): Add test to validate the route to get a specific todolist

--- server/tests/todolist.test.js
+++ server/tests/todolist.test.js
@@ -1,5 +1,6 @@
 import supertest from 'supertest'
 import { app } from '../src/app'
+import * as todolists from '../src/models/todolist'
 
 const request = supertest(app)
 
@@ -9,7 +10,7 @@ describe('Endpoints for a todolist', () => {
     const res = await request
       .post('/api/todolist')
       .send({
-        title: 'test to create a todolist',
+        title: 'test 1',
       })
     expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveProperty('success')
@@ -21,19 +22,45 @@ describe('Endpoints for a todolist', () => {
       .post('/api/todolist')
       .send()
     expect(res.statusCode).toEqual(200)
-    expect(res.body).toHaveProperty("success")
-    expect(res.body).toHaveProperty("errors")
+    expect(res.body).toHaveProperty('success')
+    expect(res.body).toHaveProperty('errors')
     expect(res.body.success).toBe(false)
+  })
+
+  it('should create a new todolist', async () => {
+    const res = await request
+      .post('/api/todolist')
+      .send({
+        title: 'test 2',
+      })
+
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toHaveProperty('success')
+    expect(res.body.success).toBe(true)
+    expect(res.body).toHaveProperty('id')
+
+    const result = await request
+      .get(`/api/todolist/${res.body.id}`)
+      .send()
+
+    expect(result.statusCode).toBe(200)
+    expect(result.body).toHaveProperty('success')
+    expect(result.body.success).toBe(true)
+    expect(result.body).toHaveProperty('todolist')
+    expect(result.body.todolist.title).toBe('test 2')
   })
 
   it('should get all the todolists', async () => {
     const res = await request
       .get('/api/todolist')
       .send()
-    
+
     expect(res.statusCode).toBe(200)
-    expect(res.body).toHaveProperty("success")
+    expect(res.body).toHaveProperty('success')
     expect(res.body.success).toBe(true)
-    expect(res.body).toHaveProperty("todolists")
+    expect(res.body).toHaveProperty('todolists')
+    expect(Array.isArray(res.body.todolists)).toBe(true)
+    expect(res.body.todolists.length).toBe(2)
   })
+
 })
